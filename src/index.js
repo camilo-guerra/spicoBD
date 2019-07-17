f8d8613eabc33173861db33ff706690a3b5f2f89
unknown <camilo.guerra.gonzalez@gmail.com>
2019-07-16 20:58:34 -0500

    mi primer commit con la estructura basica para escuchar por el puerto 1500

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const morgan = require ('morgan');
+
+const app = express();
+
+
+app.set('port',process.env.PORT || 1500);
+
+
+app.use(morgan('dev'));
+
+
+//variables globales
+
+//routes
+
+// public
+
+
+//starting de server
+app.listen(app.get('port'),()=>{
+
+    console.log('server on port ', app.get('port'));
+
+});
