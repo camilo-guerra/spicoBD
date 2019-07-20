62a861c007a2172eed11ce535a43d6999e46ba53
unknown <camilo.guerra.gonzalez@gmail.com>
2019-07-20 15:50:53 -0500

    signd

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,11 +2,11 @@ const express = require ('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.get('/singup',(req,res)=>{
-res.render('auth/singup');
-});
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+  });
 
-router.post ('signup',(req,res)=>{
+router.post ('/signup',(req,res)=>{
 
 passport.authenticate('local.singup',{
     successRedirect:'/profile',
