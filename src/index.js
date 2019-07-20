0079c7ac72754fca33a17a1e6cb817852857b825
unknown <camilo.guerra.gonzalez@gmail.com>
2019-07-20 08:46:55 -0500

    revisión de codigo

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ app.use('/links', require('./routes/links'));
 
 // public
 
-app.use(express(static(path.join(__dirname,'public'))));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 
