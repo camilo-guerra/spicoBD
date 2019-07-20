e0a12e8abfc97d0296a8d6b34e9b43742dec04fc
unknown <camilo.guerra.gonzalez@gmail.com>
2019-07-20 12:52:37 -0500

    cambio par alistar

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,35 +1,35 @@
-const express = require ('express');
+const express = require('express');
 
 const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/add',(req,res)=>{
+router.get('/add', (req, res) => {
 
-res.render('links/add');
-
-});
-
-router.get('/',async (req,res)=>{
-   
-
-const usuarios = await pool.query('select * from  usuarios');
-
-res.render('/links/list',{usuarios});
-
-
+    res.render('links/add');
 
 });
 
-router.post('/add',async (req,res)=>{
-    const{idusuarios,full_name} = req.body;
+router.get('/', async (req, res) => {
+
+
+    const usuarios = await pool.query('select * from  usuarios');
+
+    res.render('links/list', { usuarios });
+
+
+
+});
+
+router.post('/add', async (req, res) => {
+    const { idusuarios, full_name } = req.body;
     const newLink = {
-idusuarios,
-full_name
+        idusuarios,
+        full_name
     };
-console.log(newLink);
+    console.log(newLink);
 
-await pool.query('insert into usuarios set ?', [newLink]);
+    await pool.query('insert into usuarios set ?', [newLink]);
     res.send('recibido');
 
 
